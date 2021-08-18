f866d0bda731193d0d4f042bb79ca120a2913a5c
utorner <ulises@truehome.com.mx>
2021-08-18 18:48:58 -0500

    Update: AboutView: added styling and icons

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import { Header } from "../../Components";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIcons, faCodeBranch } from '@fortawesome/free-solid-svg-icons'
+
 const About = () => {
   return (
     <main>
-      <Header title="About" />
+      <Header title="About this app" />
       <section>
         <h3>Hello there!</h3>
         <p>
           This app was created using the Create-React-App tool, npm, webapack, and of course React.
         </p>
         <p>
-          Unit testing done with enzyme
+          Developed in 2021 by Ulises Torner Campuzano
         </p>
       </section>
+
       <section>
         <h3>Get the code</h3>
         <p>
-          Link to Github pending..
+          <FontAwesomeIcon icon={faCodeBranch} />
+          Link to Github pending...
         </p>
       </section>
 
       <section>
         <h3>Librerías y herramientas notables</h3>
         <p>
+          <FontAwesomeIcon icon={faIcons} />
           Íconos por <a href="https://fontawesome.com/">Font Awesome</a>
         </p>
       </section>
