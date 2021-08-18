bca1dc48011c22c5962cca133bce7f4a1938e41e
utorner <ulises@truehome.com.mx>
2021-08-18 12:01:35 -0500

    New: constants: added property types and classes joiner function

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,20 @@
+export const classes = (...classList) => classList.filter(c => c).join("  ");
+
+export const PROPERTY_TYPES = [
+  {
+    id: 1,
+    name: "House",
+  },
+  {
+    id: 2,
+    name: "Apartment",
+  },
+  {
+    id: 3,
+    name: "Lot",
+  },
+];
+
 export const properties = [
   {
     id: 1,
