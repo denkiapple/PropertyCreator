4dea6c52ab77a47de023181c7de8131c874c491d
utorner <ulises@truehome.com.mx>
2021-08-21 00:09:52 -0500

    New: NumberInput: Created component, styling and testfile

---
+++ src/Components/NumberInput/NumberInput.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { func, number, string } from "prop-types";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faPlus,
+  faMinus,
+} from '@fortawesome/free-solid-svg-icons'
+
+import styles from "./NumberInput.module.css";
+
+const NumberInput = ({ title, value, onChange }) => {
+  const increment = () => {
+    onChange(value + 1);
+  };
+
+  const decrement = () => {
+    onChange(value - 1);
+  };
+
+  return (
+    <div className={styles.layout}>
+      <p className={styles.title}>
+        {title}
+      </p>
+
+      <div className={styles.container}>
+        <button className={styles.leftBtn} onClick={decrement}>
+          <FontAwesomeIcon icon={faMinus} className={styles.navIcon} />
+        </button>
+        <span className={styles.value}>{value || 0}</span>
+        <button className={styles.rightBtn} onClick={increment}>
+          <FontAwesomeIcon icon={faPlus} className={styles.navIcon} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+NumberInput.propTypes = {
+  title: string,
+  value: number,
+  onChange: func,
+};
+
+NumberInput.defaultProps = {
+  title: "Number Input",
+  value: 0,
+  onChange: () => {},
+};
+
+export default NumberInput;
