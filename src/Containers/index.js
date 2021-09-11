147930deaa91e72e5e4cbccef0c3fd509ae18654
utorner <ulises@truehome.com.mx>
2021-09-10 23:30:48 -0500

    Update: Multiples: Updated styles

--- src/Containers/index.js
+++ src/Containers/index.js
@@ -1,5 +1,4 @@
 export { default as About } from "./About/About";
-export { default as Dashboard } from "./Dashboard/Dashboard";
 export { default as PropertyForm } from "./PropertyForm/PropertyForm";
 
 export { default as AddPropertyContainer } from "./AddPropertyContainer";
