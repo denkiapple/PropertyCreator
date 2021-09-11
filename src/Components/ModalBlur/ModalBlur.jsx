01a43047f0b0164659fbb7dcc54505f7e88f2616
utorner <ulises@truehome.com.mx>
2021-09-10 23:31:41 -0500

    Update: ModalBlur: created header, close and blur eventListener

--- src/Components/ModalBlur/ModalBlur.jsx
+++ src/Components/ModalBlur/ModalBlur.jsx
@@ -1,14 +1,43 @@
-import React from "react";
-import { oneOfType, arrayOf, element } from "prop-types";
+import React, { useEffect, useRef } from "react";
+import { oneOfType, arrayOf, element, func, string } from "prop-types";
 
 import styles from "./ModalBlur.module.css";
 
-const ModalBlur = ({ children }) => {
+const ModalBlur = ({ title, closeLabel, children, onClose }) => {
+  const wrapperRef = useRef(null);
+  useEffect(() => {
+    // Alert if clicked on outside of element
+    const handleClickOutside = event => {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        alert("You clicked outside of me!");
+        onClose();
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose, wrapperRef]);
+
+  const handleClose = () => {
+    onClose();
+  };
+
   return (
     <>
       {children && (
-        <div className={styles.layout}>
-          {children}
+        <div className={styles.layout} ref={wrapperRef}>
+          <header className={styles.header}>
+            <h2>{title}</h2>
+            <button type="button" onClick={handleClose}>{closeLabel}</button>
+          </header>
+
+          <div className={styles.content}>
+            {children}
+          </div>
         </div>
       )}
       <div className={styles.overlay}></div>
@@ -17,11 +46,17 @@ const ModalBlur = ({ children }) => {
 };
 
 ModalBlur.propTypes = {
+  title: string,
+  closeLabel: string,
   children: oneOfType([ element, arrayOf(element) ]),
+  onClose: func,
 };
 
 ModalBlur.defaultProps = {
+  title: "Modal title",
+  closeLabel: "Close",
   children: {},
+  onClose: () => {},
 };
 
 export default ModalBlur;
